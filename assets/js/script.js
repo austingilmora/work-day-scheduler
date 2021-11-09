1362002a324c67b01ba1679d2f61b8266f63b63a
Austin Gilmore <austingilmora@gmail.com>
2021-11-09 14:35:37 -0500

    added code to load text, as well as color code the hours of the day

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,30 @@ saveButton.on("click", function() {
     //save that to local storage
     localStorage.setItem(id, text);
 });
+
+function loadText() {
+    // get the current hour
+    var currentHour = moment().hour();
+
+    //starting at 7am, up till 19pm 
+    for (let i = 7; i < 20; i++) {
+        // get the text from local storage
+        var text = localStorage.getItem(i);
+        // set that id with the corresponding text
+        $("#" + i).val(text);
+        // if the current hour is less than i, it's in the future
+        if (currentHour < i) {
+            $("#" + i).addClass("future");
+        }
+        // if it's the same, it's the present 
+        else if (currentHour === i) {
+            $("#" + i).addClass("present");
+        }
+        // if it's more than i, it's in the past
+        else if (currentHour > i) {
+            $("#" + i).addClass("past");
+        }
+    }
+};
+
+loadText();
